cec017309074619712f8be2a42427f601ef31915
Zak Ali <zak.ali@thetin.net>
2021-03-15 00:26:30 +0000

    Add signup mutation to clientside and add basic validation and error displays

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
 
 export default class Signin extends Component {
+    state = {
+        form: {
+            username: '',
+            email: '',
+            password: '',
+            confirm_password: ''
+        }
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    
+
+
     render() {
         return (
-            <div className="auth-area signin">
+            <div className="auth-area signin container">
                 <h1>Signin</h1>
                 <form className="form">
                     <label htmlFor="username">Username</label>
-                    <input name="username" type="text" value={this.form.username} id="username" placeholder="Enter username" />
+                    <input name="username" type="text" value={this.state.form.username} id="username" placeholder="Enter username" onChange={this.handleInputChange}/>
 
                     <label htmlFor="password">Password</label>
-                    <input name="password" type="password" value={this.form.password} id="password" placeholder="Enter password" />
+                    <input name="password" type="password" value={this.state.form.password} id="password" placeholder="Enter password" onChange={this.handleInputChange}/>
             
-                    <button className="btn btn--primary" type="submit">Signup</button>
+                    <button className="btn btn--primary" type="submit">Sigin</button>
                 </form>
             </div>
         )
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,25 +1,95 @@
 import React, { Component } from 'react'
+import {Mutation} from "react-apollo"
+import SIGN_UP from "../mutations/signup"
 
 export default class Signup extends Component {
+    state = {
+        form: {
+            username: '',
+            email: '',
+            password: '',
+            confirm_password: ''
+        },
+        error: null
+    }
+
+    handleInputChange = (e) => {
+        this.setState((state) => ({
+            form: {
+                ...state.form, 
+                [e.target.name]: e.target.value
+            }
+        }))
+    }
+
+    handleSubmit = (e, signupUserFunc) => {
+        e.preventDefault();
+        const {username, email, password, confirm_password} = this.state.form;
+        if(password === confirm_password){
+            try {
+                this.setState({
+                    error: null
+                })
+                console.log("Signing in....")
+                signupUserFunc({
+                    variables: {
+                        username, 
+                        email, 
+                        password
+                    }
+                }).catch(err => console.log(err))
+            } catch(e){
+                console.log(e)
+            }
+        } else {
+            this.setState({
+                error: {
+                    message: 'Passwords do not match'
+                }
+            })
+        }
+    }
+    
+    renderGraphqlErrors = (graphQLErrors) => {
+        return (
+            <div>
+                {graphQLErrors.map((err, index) => (
+                    <p key={index} style={{color: 'red'}}>{err.message}</p>
+                ))}
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div className="auth-area signup">
+            <div className="auth-area signup container">
                 <h1>Signup</h1>
-                <form className="form">
-                    <label htmlFor="username">Username</label>
-                    <input name="username" type="text" value={this.form.username} id="username" placeholder="Enter username" />
+                <Mutation mutation={SIGN_UP}>
+                    {(signupUser, {error}) => {
+                        console.log(error)
+                        return (
+                            <React.Fragment>
+                                {error && this.renderGraphqlErrors(error.graphQLErrors)}
+                                {this.state.error && <p style={{color: 'red'}}>{this.state.error.message}</p>}
+                                <form className="form" onSubmit={e => this.handleSubmit(e, signupUser)}>
+                                    <label htmlFor="username">Username</label>
+                                    <input name="username" type="text" value={this.state.form.username} id="username" placeholder="Enter username" onChange={this.handleInputChange}/>
 
-                    <label htmlFor="email">Email</label>
-                    <input name="email" type="email" value={this.form.email} id="email" placeholder="Enter email" />
+                                    <label htmlFor="email">Email</label>
+                                    <input name="email" type="email" value={this.state.form.email} id="email" placeholder="Enter email" onChange={this.handleInputChange}/>
 
-                    <label htmlFor="password">Password</label>
-                    <input name="password" type="password" value={this.form.password} id="password" placeholder="Enter password" />
+                                    <label htmlFor="password">Password</label>
+                                    <input name="password" type="password" value={this.state.form.password} id="password" placeholder="Enter password" onChange={this.handleInputChange}/>
 
-                    <label htmlFor="confirm_password">Confirm Password</label>
-                    <input name="confirm_password" type="password" value={this.form.confirm_password} id="confirm_password" placeholder="Enter confirm password" /> 
-            
-                    <button className="btn btn--primary" type="submit">Signup</button>
-                </form>
+                                    <label htmlFor="confirm_password">Confirm Password</label>
+                                    <input name="confirm_password" type="password" value={this.state.form.confirm_password} id="confirm_password" placeholder="Enter confirm password" onChange={this.handleInputChange}/> 
+                            
+                                    <button className="btn btn--primary" type="submit">Signup</button>
+                                </form>
+                            </React.Fragment>
+                        )
+                    }}
+                </Mutation>
             </div>
         )
     }
